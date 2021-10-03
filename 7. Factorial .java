2885cb18d86d819f3bca0998851038effad9c8c6
Aman Jain <ajjaina.jain@gmail.com>
2021-10-03 23:58:20 +0530

    Factorial
    7. Factorial 
    Given an integer N. Write a program to return the factorial of N.
    Note: 0 factorial is equal to 1.
    
    Example 1:
    Input:
    N = 10
    Output: 
    3628800
    Explanation:
    1*2*3*4*5*6*7*8*9*10 = 3628800 .
    
    Your Task:
    The input N is provided as a parameter to the function nFactorial. Complete the given code so that it returns the factorial of N. The output is printed by the driver code.

---
+++ 7. Factorial .java
@@ -0,0 +1,37 @@
+//Initial Template for Java
+
+import java.util.Scanner;
+
+class Main {
+
+    
+ // } Driver Code Ends
+//User function Template for Java
+
+public static int nFactorial(int n){
+    int ans = 1;
+    
+    //Write your code here
+     while(n>0)
+    {
+        ans = ans*n;
+        n--;
+    }
+    
+    return ans;
+}
+
+// { Driver Code Starts.
+
+    public static void main(String[] args)
+    {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while(t-- > 0) {
+            int n = scn.nextInt();
+            int ans = nFactorial(n);
+            System.out.println(ans);
+        }
+        scn.close();
+    }
+}  // } Driver Code Ends
